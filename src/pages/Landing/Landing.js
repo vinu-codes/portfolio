a5c234792fff556e5df3fcbd3ad726f5e1ed7001
Vinu <vinujithmin@gmail.com>
2023-12-22 21:06:17 +1100

    create new landingpage

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,11 +1,5 @@
 import React, { useState } from 'react'
 
-import { TechRender } from '@components/TechCard'
-import { Button } from '@common/Button'
-import { Dropdown } from '@common/Dropdown/Dropdown'
-import { Toggle } from '@common/Toggle'
-import { Accordion } from '@common/Accordion'
-import { Provider as AccordionContainer } from '@common/Accordion'
 import { Layout } from '@components/Layout'
 import { Main } from '@common/Main'
 
--- src/pages/Landing/shared/Library/Library.js
+++ src/pages/Landing/shared/Library/Library.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import { TechRender } from '@components/TechCard'
+import { Button } from '@common/Button'
+import { Dropdown } from '@common/Dropdown/Dropdown'
+import { Toggle } from '@common/Toggle'
+import { Accordion } from '@common/Accordion'
+import { Provider as AccordionContainer } from '@common/Accordion'
+
 const Library = () => {}
 
 export { Library }
